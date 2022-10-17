49cb64e33df1585407e3af1c92d42291ec661234
akmal <akmal@DESKTOP-QEPVMLM>
2022-10-17 23:45:12 +0700

    First Commit

---
+++ src/main/java/kelompoknya/akmal/learningmigratedb/LearningmigratedbApplication.java
@@ -0,0 +1,13 @@
+package kelompoknya.akmal.learningmigratedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmigratedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningmigratedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kelompoknya/akmal/learningmigratedb/LearningmigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package kelompoknya.akmal.learningmigratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
